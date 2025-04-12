85d742203a7b7500e0d51725963a0db635ac26f4
Ema998 <emanuele.gornati3@gmail.com>
2025-04-12 13:46:40 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php 
+trait Calculator{
+    public function sum($a, $b) {
+        return $a + $b;
+    }
+    
+    public function sub($a, $b) {
+        return $a - $b;
+    }
+    
+    public function mul($a, $b) {
+        return $a * $b;
+    }
+    
+    public function div($a, $b) {
+        return $a / $b;
+    }
+    
+    public function sqr($a){
+        return sqrt($a);
+    }
+}
+
+class Rettangolo {
+    use Calculator;
+    
+    private $base;
+    private $altezza;
+    
+    public function __construct($base, $altezza) {
+        $this->base = $base;
+        $this->altezza = $altezza;
+    }
+    
+    public function area() {
+        return $this->mul($this->base, $this->altezza);
+    }
+    
+    public function perimetro() {
+        return $this->sum($this->mul(2, $this->base), $this->mul(2, $this->altezza));
+    }
+
+    public function diagonale() {
+        return $this->sqr($this->sum($this->sqr($this->base), $this->sqr($this->altezza)));
+    }
+}
+
+?>
